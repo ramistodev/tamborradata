69cfbfb4444d3c02b50199b4579fbf43d08527e6
ramistodev <168463169+ramistodev@users.noreply.github.com>
2025-11-03 22:40:28 +0100

    feat: Todo listo o eso espero

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        destination: 'https://tamborradata.com/$1',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
