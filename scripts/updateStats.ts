6f402f0b7a7f6ec3956e5eb4fc1691b6ed6e3400
ramistodev <168463169+ramistodev@users.noreply.github.com>
2025-11-05 17:40:16 +0100

    feat: Prompts arreglados

--- scripts/updateStats.ts
+++ scripts/updateStats.ts
@@ -1,14 +1,14 @@
-// import { collectUrls } from './logic/url/collectUrls';
-// import { collectParticipants } from './logic/participants/collectParticipants';
+import { collectUrls } from './logic/url/collectUrls';
+import { collectParticipants } from './logic/participants/collectParticipants';
 import { collectStatistics } from './logic/statistics/collectStatistics';
 
 export async function updateStats() {
   // Lógica para actualizar las estadísticas de la Tamborrada, esto se ejecutara cada cierto tiempo
 
   // Conseguir URLs
-  // await collectUrls();
+  await collectUrls();
 
-  // await collectParticipants();
+  await collectParticipants();
 
   await collectStatistics();
 }
