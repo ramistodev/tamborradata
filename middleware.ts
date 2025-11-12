0dd52830488f5e49648c6e0f7ac4c4912cb72322
ramistodev <168463169+ramistodev@users.noreply.github.com>
2025-11-12 19:31:33 +0100

    feat: Middleware arreglado

--- middleware.ts
+++ middleware.ts
@@ -4,16 +4,12 @@ export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   if (!pathname.startsWith('/api')) return NextResponse.next();
 
-  const host = req.headers.get('host');
-  if (
-    host?.includes('tamborradata.com') ||
-    host?.includes('vercel.app') ||
-    host?.includes('localhost')
-  ) {
-    return NextResponse.next();
+  const accept = req.headers.get('accept') || '';
+  if (accept.includes('text/html')) {
+    return new NextResponse('Forbidden', { status: 403 });
   }
 
-  return new NextResponse('Forbidden!', { status: 403 });
+  return NextResponse.next();
 }
 
 export const config = {
