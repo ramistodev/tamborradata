36577281e979b118f27b6ad5a2b470a4e883d18f
ramistodev <168463169+ramistodev@users.noreply.github.com>
2025-11-18 12:06:01 +0100

    fix: Problema con el sitemap arreglado

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next';
 import { fetchYears } from './(frontend)/services/fetchYears';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '';
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://tamborradata.com';
   const currentYear = new Date().getFullYear();
 
   const years = await fetchYears(`${baseUrl}/api/available-years`);
